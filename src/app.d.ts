908d42496ecc9601b0be4755a0d523f117fd0f32
Krist Ponpairin <krist7599555@gmail.com>
2026-01-11 01:57:54 +0700

    feat: setup formatter

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,20 +1,28 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
-		interface Platform {
-			env: Env;
-			ctx: ExecutionContext;
-			caches: CacheStorage;
-			cf?: IncomingRequestCfProperties;
-		}
+    namespace App {
+        interface Platform {
+            env: Env;
+            ctx: ExecutionContext;
+            caches: CacheStorage;
+            cf?: IncomingRequestCfProperties;
+        }
 
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+        // interface Error {}
+        // interface Locals {}
+        // interface PageData {}
+        // interface PageState {}
+        // interface Platform {}
+    }
+    declare module '*.svx' {
+        const component: ConstructorOfATypedSvelteComponent;
+        export default component;
+    }
+    // declare module '*.svelte' {
+    //     const component: ConstructorOfATypedSvelteComponent;
+    //     export default component;
+    // }
 }
 
 export {};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<script lang='ts'>
+   import Main from './1.svx'
+</script>
+
+<div class="px-8 py-4 flex justify-between">
+  <p class="text-xs">the-krist personal blogs</p>
+  <p class="text-xs">จะเขียนอะไรที่อยากเขียน</p>
+</div>
+
+<div class="prose container mx-auto mt-8 px-8">
+  <Main />
+</div>
